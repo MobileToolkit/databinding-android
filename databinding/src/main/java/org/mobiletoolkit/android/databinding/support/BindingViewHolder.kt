55ea9ee5f5c783d143dc0ac6499fc11743b57eb3
Sebastian Owodzin <sebastian.owodzin@gmail.com>
2018-11-25 22:58:17 +0000

    added missing dependency

--- databinding/src/main/java/org/mobiletoolkit/android/databinding/support/BindingViewHolder.kt
+++ databinding/src/main/java/org/mobiletoolkit/android/databinding/support/BindingViewHolder.kt
@@ -3,6 +3,7 @@ package org.mobiletoolkit.android.databinding.support
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
